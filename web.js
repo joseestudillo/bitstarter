486052a5afdb116b458fd0e44877b3a8dc532ce0
Jose Estudillo <estudillo.jose@gmail.com>
2013-07-10 16:02:29 +0000

    Serving HTML file added

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  //TODO improve file reading	
+  fs.readFile("index.html", function (err,data) {
+    if (err) {
+    	response.writeHead(404);
+    	response.end(JSON.stringify(err));
+      return;
+    }
+    response.writeHead(200);
+    response.end(data);
+  });	
 });
 
 var port = process.env.PORT || 5000;
